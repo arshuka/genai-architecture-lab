616bd7b1d13ee90548fab12c21bfab768a0a529d
arshuka <arshuka@gmail.com>
2026-02-11 21:16:23 +0530

    Add files via upload

--- menu_config.py
+++ menu_config.py
@@ -1,43 +1,114 @@
 MENU_TREE = {
     "GenAI": {
-        "LLM": {
-            "LLM + Basics": {
-                "image": "211_llm_basic.png",
-                "image_anim": "211_llm_basic.gif",
+        "LLM-Language & Foundation Models": {
+            "LLM-Basic Model": {
+                "image1": "llm/211_llm_basic_1.png",
+                "image2": "llm/211_llm_basic_2.png",
+                "image3": "llm/211_llm_basic_3.png",
+                "text_md": "211_LLMbasic.md",
+                "trade_md": "211_LLMbasictrade.md"
+            }, 
+            "LLM-Only (API-Based) Model": {
+                "image": "llm/211_llm_basic.png",
+                "image_anim": "llm/211_llm_basic.gif",
                 "text_md": "211_LLMbasic.md",
                 "trade_md": "211_LLMbasictrade.md"
             },
-            "LLM + System Prompt": {
+            "LLM System Prompt Model": {
                 "image": "212_system_prompt.png",
                 "image_anim": "212_system_prompt.gif",
                 "text_md": "212_LLMprompt.md",
                 "trade_md": "212_LLMprompttrade.md"
             },
-            
-            "LLM + Tools": {
+             
+            "LLM Tools Model": {
                 "image": "213_LLMTools.png",
                 "image_anim": "213_LLMTools.gif",
                 "text_md": "213_LLMTools.md",
                 "trade_md": "213_LLMToolsTrade.md"
             },
             
-            "LLM + Memory": {
+            "LLM Memory Model": {
                 "image": "214_LLMMemory.png",
                 "image_anim": "214_LLMMemory.gif",
                 "text_md": "214_LLMMemory.md",
                 "trade_md": "214_LLMMemoryTrade.md"
             },
 
-            "LLM + Moderation": {
+            "LLM Moderation Model": {
                 "image": "215_LLMModernization.png",
                 "image_anim": "215_LLMModernization.gif",
                 "text_md": "215_LLMModernization.md",
                 "trade_md": "215_LLMModernizationTrade.md"
-            }
+            },
 
-            
-        },
-        "RAG": {
+            "LLM Foundation Model": {
+                "image": "215_LLMModernizatidon.png",
+                "image_anim": "215_LLMModernizdation.gif",
+                "text_md": "215_LLMModernizadtion.md",
+                "trade_md": "215_LLMModernizatiodnTrade.md"
+            },
+
+            "LLM Instruction-Tuned Model": {
+                "image": "215_LLMModernization.png",
+                "image_anim": "215_LLMModernization.gif",
+                "text_md": "215_LLMModernization.md",
+                "trade_md": "215_LLMModernizationTrade.md"
+            },
+
+            "LLM Chat-Optimized Model": {
+                "image": "215_LLMModernization.png",
+                "image_anim": "215_LLMModernization.gif",
+                "text_md": "215_LLMModernization.md",
+                "trade_md": "215_LLMModernizationTrade.md"
+            },
+             "LLM Fine-Tuned Model": {
+                "image": "215_LLMModernizatidon.png",
+                "image_anim": "215_LLMModernizdation.gif",
+                "text_md": "215_LLMModernizadtion.md",
+                "trade_md": "215_LLMModernizatiodnTrade.md"
+            },
+
+            "LLM Domain-Specific Model": {
+                "image": "215_LLMModernization.png",
+                "image_anim": "215_LLMModernization.gif",
+                "text_md": "215_LLMModernization.md",
+                "trade_md": "215_LLMModernizationTrade.md"
+            },
+
+            "LLM Task-Specialized Model": {
+                "image": "215_LLMModernization.png",
+                "image_anim": "215_LLMModernization.gif",
+                "text_md": "215_LLMModernization.md",
+                "trade_md": "215_LLMModernizationTrade.md"
+
+            },
+
+            "LLM Small Language(SLM) Model": {
+                "image": "215_LLMModernization.png",
+                "image_anim": "215_LLMModernization.gif",
+                "text_md": "215_LLMModernization.md",
+                "trade_md": "215_LLMModernizationTrade.md"
+
+            },
+
+            "LLM Parameter-Efficient Model": {
+                "image": "215_LLMModernization.png",
+                "image_anim": "215_LLMModernization.gif",
+                "text_md": "215_LLMModernization.md",
+                "trade_md": "215_LLMModernizationTrade.md"
+            },
+
+            "LLM Embedding Model": {
+                "image": "215_LLMModernization.png",
+                "image_anim": "215_LLMModernization.gif",
+                "text_md": "215_LLMModernization.md",
+                "trade_md": "215_LLMModernizationTrade.md"
+
+            },
+        },    
+    
+        "Multimodal Architectures": {
             "RAG Basics": {
                 "image": "221_rag_basic.png",
                 "image_anim": "221_llm_basic_animate.gif",
@@ -49,7 +120,92 @@ MENU_TREE = {
                 "text_md": "222_LLMbasic.md",
                 "trade_md": "222_LLMbasictrade.md"
             }
-        }
+        },
+        "Speech & Audio Architectures": {
+                "RAG Basics": {
+                        "image": "221_rag_basic.png",
+                    "image_anim": "221_llm_basic_animate.gif",
+                    "text_md": "221_LLMbasic.md",
+                    "trade_md": "221_LLMbasictrade.md"
+                },
+                "RAG + Re-ranking": {
+                    "image": "222_rag_rerank.png",
+                    "text_md": "222_LLMbasic.md",
+                    "trade_md": "222_LLMbasictrade.md"
+                }
+            },
+
+            "Advanced / Frontier Model Architectures": {
+                "RAG Basics": {
+                        "image": "221_rag_basic.png",
+                    "image_anim": "221_llm_basic_animate.gif",
+                    "text_md": "221_LLMbasic.md",
+                    "trade_md": "221_LLMbasictrade.md"
+                },
+                "RAG + Re-ranking": {
+                    "image": "222_rag_rerank.png",
+                    "text_md": "222_LLMbasic.md",
+                    "trade_md": "222_LLMbasictrade.md"
+                }
+            },
+
+            "RAG & Knowledge Systems": {
+                "RAG Basics": {
+                        "image": "221_rag_basic.png",
+                    "image_anim": "221_llm_basic_animate.gif",
+                    "text_md": "221_LLMbasic.md",
+                    "trade_md": "221_LLMbasictrade.md"
+                },
+                "RAG + Re-ranking": {
+                    "image": "222_rag_rerank.png",
+                    "text_md": "222_LLMbasic.md",
+                    "trade_md": "222_LLMbasictrade.md"
+                }
+            },
+
+            "Model Optimization Architectures": {
+                "RAG Basics": {
+                        "image": "221_rag_basic.png",
+                    "image_anim": "221_llm_basic_animate.gif",
+                    "text_md": "221_LLMbasic.md",
+                    "trade_md": "221_LLMbasictrade.md"
+                },
+                "RAG + Re-ranking": {
+                    "image": "222_rag_rerank.png",
+                    "text_md": "222_LLMbasic.md",
+                    "trade_md": "222_LLMbasictrade.md"
+                }
+           },
+
+            "Inference & Performance Architectures": {
+                "RAG Basics": {
+                        "image": "221_rag_basic.png",
+                    "image_anim": "221_llm_basic_animate.gif",
+                    "text_md": "221_LLMbasic.md",
+                    "trade_md": "221_LLMbasictrade.md"
+                },
+                "RAG + Re-ranking": {
+                    "image": "222_rag_rerank.png",
+                    "text_md": "222_LLMbasic.md",
+                    "trade_md": "222_LLMbasictrade.md"
+                }
+           },
+
+            "Deployment & Scaling Architectures": {
+                "RAG Basics": {
+                        "image": "221_rag_basic.png",
+                    "image_anim": "221_llm_basic_animate.gif",
+                    "text_md": "221_LLMbasic.md",
+                    "trade_md": "221_LLMbasictrade.md"
+                },
+                "RAG + Re-ranking": {
+                    "image": "222_rag_rerank.png",
+                    "text_md": "222_LLMbasic.md",
+                    "trade_md": "222_LLMbasictrade.md"
+                }
+           },
+          
+
     },
 
     "Welcome": {
@@ -67,26 +223,35 @@ MENU_TREE = {
         }
     },
 
-    "AI Quick Guide": {
-        "Safety": {
-            "Basics": {
-                "image": None,
-                "text": "Guardrails enforce safety and compliance."
+    "AI One-Stop": {
+        "Coming up": {
+            "Intro": {
+               "image": "overview.png",
+                "text_md": "LLMbasic.md",
+                "trade_md": "LLMbasictrade.md"
             }
         }
     },
-
+  
     "Cloud AI": {
-        "AWS_AI": {
-            "Basics": {
-                "image": None,
-                "text": "Guardrails enforce safety and compliance."
+        "Coming up": {
+            "Intro": {
+                "image": "overview.png",
+                "text_md": "LLMbasic.md",
+                "trade_md": "LLMbasictrade.md"
+            }
+        }
+    }, 
+
+    "AI Tools": {
+        "Coming up": {
+            "Intro": {
+                "image": "overview.png",
+                "text_md": "LLMbasic.md",
+                "trade_md": "LLMbasictrade.md"
             }
         }
     }
 
     # AI Quick Guide
 }
-
-
-
