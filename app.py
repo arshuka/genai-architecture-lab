0cfb3b8644ef0add81e2912de36ee6b4b1dd5d2c
arshuka <arshuka@gmail.com>
2026-02-13 10:49:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -361,8 +361,8 @@ def log_ask_ai(user_id, question, page_context):
 
 
 def get_backend_url():
-     dev_mode = os.getenv("DEV_MODE", "").lower()
-            
+    dev_mode = os.getenv("DEV_MODE", "").lower()
+
     if dev_mode == "true":
         return os.getenv("AUTH_BACKEND_URL")
 
@@ -500,17 +500,24 @@ with topbar:
             </div>
             """, unsafe_allow_html=True)
         else:
-             dev_mode = os.getenv("DEV_MODE", "").lower()
+            dev_mode = os.getenv("DEV_MODE", "").lower()
 
             st.markdown(f"""
                 <div class="top-account">
-                    <span class="acct-name">{dev_mode}Guestsssssss</span>
+                    <span class="acct-name">{dev_mode} Guestsfsfsfsf</span>
                 </div>
-             """, unsafe_allow_html=True)
+            """, unsafe_allow_html=True)
 
+
+            if dev_mode == "true":
+                AUTH_BACKEND_URL =os.getenv("AUTH_BACKEND_URL")
+            else:
+                AUTH_BACKEND_URL =os.getenv("AUTH_BACKEND_URL")
+
+ 
             st.markdown(f"""
                 <div class="top-account">
-                    <span class="acct-name">{AUTH_BACKEND_URL}Guest</span>
+                    <span class="acct-name">Guest</span>
                     <a href="{AUTH_BACKEND_URL}/auth/google" class="acct-link" target="_top">Login</a>
                 </div>
             """, unsafe_allow_html=True)
@@ -1169,7 +1176,3 @@ with col_right:
                 trade_text = trade_path.read_text().strip()
                 if trade_text:
                     st.markdown(trade_text)
-
-
-
-
