06ba137d2f18e4490a4ac20657cc8a30742a3be8
arshuka <arshuka@gmail.com>
2026-02-13 11:14:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -518,7 +518,7 @@ with topbar:
   
             st.markdown(f"""
                 <div class="top-account">
-                    <span class="acct-name">{AUTH_BACKEND_URL}Guest</span>
+                    <span class="acct-name">{AUTH_BACKEND_URL}Guest hecking</span>
                     <a href="{AUTH_BACKEND_URL}/auth/google" class="acct-link" target="_top">Login</a>
                 </div>
             """, unsafe_allow_html=True)
